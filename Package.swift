31121000ba5e87475135de03cc4b59e6e3ef7512
Ian Loveshack <ians325@aol.com>
2026-05-01 18:16:40 +0100

    Fix visionOS platform version syntax for swift-tools 5.9

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "FFmpegKit",
     defaultLocalization: "en",
     platforms: [.macOS(.v10_15), .macCatalyst(.v14), .iOS(.v13), .tvOS(.v13),
-                .visionOS(.v2)],
+                .visionOS("2.0")],
     products: [
         .library(
             name: "FFmpegKit",
